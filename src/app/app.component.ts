350a30cf63be1121c18dba2c2a0e9261691b086c
Sumit Kumar Jha <jhasumit4742@gmail.com>
2025-02-06 15:13:53 +0530

    Made the application resposive for all devices

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,18 @@ import { Component } from '@angular/core';
 import { InputFormComponent } from './input-form/input-form.component';
 import { TableComponent } from './table/table.component';
 import { ChartComponent } from './chart/chart.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [InputFormComponent, TableComponent, ChartComponent],
+  imports: [FormsModule, InputFormComponent, TableComponent, ChartComponent],
   templateUrl: './app.component.html',
 })
 export class AppComponent {
   title = 'fylehealth';
+  menuOpen = false; // Controls sidebar visibility
+
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+  }
 }
